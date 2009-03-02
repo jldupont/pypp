b8371da83e1f07ed3a26a1d62063b6ec1ae81563
jeanlou.dupont <jeanlou.dupont@f0c58a34-0500-11de-83f3-6f0f76eb4d42>
2009-03-02 16:16:56 +0000

    Divided work amongst classes

--- trunk/pypp/loader.py
+++ trunk/pypp/loader.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
-"""
+""" Module Loader class
+    project: pypp
+    
     @author: Jean-Lou Dupont
 """
-
 __author__  = "Jean-Lou Dupont"
 __version__ = "$Id$"
+
+__all__ = ['Loader']
+
+import imp
+import sys
 
 class Loader(object):
     """ Loads a .pypp file in place of the usual .py
     """
-    def __init__(self):
-        pass
+    def __init__(self, name, file, pathname, desc, scope):
+        self.file = file
+        self.name = name
+        self.pathname = pathname
+        self.desc = desc
+        self.scope = scope
+
+    def load_module(self, fullname):
+        if fullname in __builtins__:
+            try:
+                mod = imp.load_module(self.name, self.file,
+                                      self.pathname, self.desc)
+            finally:
+                if self.file:
+                    self.file.close()
+            sys.modules[fullname] = mod
+        else:
+            if self.file:
+                self.file.close()
+            mod = makeImportedModule(self.name, self.pathname, self.desc,
+                                     self.scope)
+            sys.modules[fullname] = mod
+        return mod
     
 
 # ==============================================
